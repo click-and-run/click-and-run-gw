1b7db1b6db4c5efcff30edffbfd0495684c3756c
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-05-12 12:27:55 +0200

    add spreadsheet handle to receive file

---
+++ src/main/webapp/app/spreadsheet/spreadsheet-handler/spreadsheet-handler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-spreadsheet-handler',
+  templateUrl: './spreadsheet-handler.component.html',
+  styles: []
+})
+export class SpreadsheetHandlerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/main/webapp/app/spreadsheet/spreadsheet.module.ts
+++ src/main/webapp/app/spreadsheet/spreadsheet.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpreadsheetComponent } from './spreadsheet.component';
+import { SpreadsheetHandlerComponent } from './spreadsheet-handler/spreadsheet-handler.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        SpreadsheetComponent
+        SpreadsheetComponent,
+        SpreadsheetHandlerComponent
     ], exports: [
         SpreadsheetComponent
     ]
