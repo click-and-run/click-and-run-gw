0d5386d08a3a8b72f4a66d47f91190a7dcea9bb4
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-05-16 09:25:11 +0200

    handle http errors

--- src/main/webapp/app/spreadsheet/spreadsheet-validation/spreadsheet-validation.component.ts
+++ src/main/webapp/app/spreadsheet/spreadsheet-validation/spreadsheet-validation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SpreadsheetService } from '../spreadsheet.service';
 import { WorkbookValidationModel } from '../models/validation.model';
 import { Subscription } from 'rxjs';
+import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-spreadsheet-validation',
@@ -10,19 +11,27 @@ import { Subscription } from 'rxjs';
 })
 export class SpreadsheetValidationComponent implements OnInit, OnDestroy {
     public workbookValidation: WorkbookValidationModel;
-    private sub: Subscription;
+    private subs: Array<Subscription> = new Array<Subscription>();
 
-    constructor(private spreadsheetService: SpreadsheetService) {
+    constructor(private spreadsheetService: SpreadsheetService, private eventManager: JhiEventManager, private alertService: JhiAlertService) {
     }
 
     ngOnInit() {
-        this.sub = this.spreadsheetService.sharedValidation.subscribe((wbv) => {
-            this.workbookValidation = wbv;
-        });
+        this.subs.push(
+            this.spreadsheetService.sharedValidation.subscribe((wbv) => {
+                this.workbookValidation = wbv;
+            }),
+            this.eventManager.subscribe('clickandrungwApp.httpError', (error) => {
+                if (error.status === 400) {
+                    this.alertService.error('Request failed, please retry with different arguments.');
+                } else {
+                    this.alertService.error('Mmh, It seems you found an undesired feature. We sent this "surprise" to the IT team.')
+                }
+            }));
     }
 
     ngOnDestroy() {
-        this.sub.unsubscribe();
+        this.subs.forEach((sub) => sub.unsubscribe());
     }
 
     getSheetValidations() {
