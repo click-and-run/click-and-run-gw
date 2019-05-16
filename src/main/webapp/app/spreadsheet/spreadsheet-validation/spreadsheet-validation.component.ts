b7331c957d7b6f6d6dd10980d0b79d555b26a7ff
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-05-16 09:40:53 +0200

    handle http errors from service rather than component

--- src/main/webapp/app/spreadsheet/spreadsheet-validation/spreadsheet-validation.component.ts
+++ src/main/webapp/app/spreadsheet/spreadsheet-validation/spreadsheet-validation.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SpreadsheetService } from '../spreadsheet.service';
 import { WorkbookValidationModel } from '../models/validation.model';
 import { Subscription } from 'rxjs';
-import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-spreadsheet-validation',
@@ -13,20 +12,13 @@ export class SpreadsheetValidationComponent implements OnInit, OnDestroy {
     public workbookValidation: WorkbookValidationModel;
     private subs: Array<Subscription> = new Array<Subscription>();
 
-    constructor(private spreadsheetService: SpreadsheetService, private eventManager: JhiEventManager, private alertService: JhiAlertService) {
+    constructor(private spreadsheetService: SpreadsheetService) {
     }
 
     ngOnInit() {
         this.subs.push(
             this.spreadsheetService.sharedValidation.subscribe((wbv) => {
                 this.workbookValidation = wbv;
-            }),
-            this.eventManager.subscribe('clickandrungwApp.httpError', (error) => {
-                if (error.status === 400) {
-                    this.alertService.error('Request failed, please retry with different arguments.');
-                } else {
-                    this.alertService.error('Mmh, It seems you found an undesired feature. We sent this "surprise" to the IT team.')
-                }
             }));
     }
 
--- src/main/webapp/app/spreadsheet/spreadsheet.service.ts
+++ src/main/webapp/app/spreadsheet/spreadsheet.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Http } from '@angular/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { WorkbookValidationModel } from './models/validation.model';
+import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
 
 @Injectable()
-export class SpreadsheetService {
+export class SpreadsheetService implements OnDestroy {
+
     private apiUrl = 'http://127.0.0.1:8081/api';
     public sharedValidation = new BehaviorSubject(undefined);
+    private sub: Subscription;
 
-    constructor(private http: Http) {
+    constructor(private http: Http, private eventManager: JhiEventManager, private alertService: JhiAlertService) {
+        this.sub = this.eventManager.subscribe('clickandrungwApp.httpError', (error) => {
+            if (error.status === 400) {
+                this.alertService.error('Request failed, please retry with different arguments.');
+            } else {
+                this.alertService.error('Mmh, It seems you found an undesired feature. We sent this "surprise" to the IT team.')
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 
     public validate(resource, file): Observable<WorkbookValidationModel> {
